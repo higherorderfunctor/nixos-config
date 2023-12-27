70e7882260b7681b6e4998e6dc7ed8efa531eb29
Christopher Aubut <christopher@aubut.me>
2023-12-26 22:42:13 -0700

    chore: save

--- modules/nixos/config/disk-config.nix
+++ modules/nixos/config/disk-config.nix
@@ -24,6 +24,7 @@ _: {
               size = "100%";
               content = {
                 type = "luks";
+                name = "cryptlvm";
                 content = {
                   type = "lvm_pv"; # LVM physical volume
                   vg = "pool";
