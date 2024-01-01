4f91487a389ebbd55ff45235bed4eada1cefbea4
Christopher Aubut <christopher@aubut.me>
2024-01-01 13:39:38 -0700

    asdf

--- hosts/vm/disk-config.nix
+++ hosts/vm/disk-config.nix
@@ -3,6 +3,10 @@
     (import ../common/optional/root-luks-lvm.nix {keyFile = config.sops.secrets.vm-luks.path;})
   ];
 
+  sops.secrets.vm-luks = {
+    sopsFile = ../../secrets.yaml;
+  };
+
   # set the root device
   disko.devices.disk.root.device = "/dev/nvme0n1";
 }
