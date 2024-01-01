407ad6a009154fbec81d3dff55497cada0379400
Christopher Aubut <christopher@aubut.me>
2023-12-31 23:09:17 -0700

    update

--- hosts/beelink-ser7/hardware-configuration.nix
+++ hosts/beelink-ser7/hardware-configuration.nix
@@ -1,5 +1,27 @@
-{...}: {
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}: {
   imports = [
     ../common/optional/minimal-x86_64-linux-hardware-configuration.nix
+    (modulesPath + "/installer/scan/not-detected.nix")
   ];
+
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "thunderbolt"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+      ];
+      kernelModules = ["dm-snapshot"];
+    };
+    kernelModules = ["kvm-amd"];
+  };
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
