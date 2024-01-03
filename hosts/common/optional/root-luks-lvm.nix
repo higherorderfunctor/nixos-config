7a2917eb0383d1fd49db130d13036eac9c995542
Christopher Aubut <christopher@aubut.me>
2024-01-02 19:14:00 -0700

    fix: (untested) fixes owner of secrets file

--- hosts/common/optional/root-luks-lvm.nix
+++ hosts/common/optional/root-luks-lvm.nix
@@ -5,28 +5,29 @@
   ...
 }: let
   rollback = ''
-    mkdir /btrfs_tmp
-    mount /dev/root_vg/root /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    mkdir /btrfs-tmp
+    mount -t btrfs /dev/mapper/cryptlvm-root /btrfs-tmp
+
+    if [[ -e /btrfs-tmp/root ]]; then
+        mkdir -p /btrfs-tmp/old-roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs-tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs-tmp/root "/btrfs-tmp/old-roots/$timestamp"
     fi
 
     delete_subvolume_recursively() {
         IFS=$'\n'
         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
+            delete_subvolume_recursively "/btrfs-tmp/$i"
         done
         btrfs subvolume delete "$1"
     }
 
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+    for i in $(find /btrfs-tmp/old-roots/ -maxdepth 1 -mtime +30); do
         delete_subvolume_recursively "$i"
     done
 
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
+    btrfs subvolume create /btrfs-tmp/root
+    umount /btrfs-tmp
   '';
   phase1Systemd = config.boot.initrd.systemd.enable;
 in {
