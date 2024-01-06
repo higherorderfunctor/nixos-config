1f639763c76b34f4b62fafdc047c0d765fc104f2
Christopher Aubut <christopher@aubut.me>
2024-01-06 10:18:14 -0700

    disable vim for time

--- home/caubut/features/neovim/default.nix
+++ home/caubut/features/neovim/default.nix
@@ -8,7 +8,7 @@
     package = pkgs.neovim-nightly;
     coc.enable = false;
   };
-  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/${config.home.username}/Documents/nixos-config/home/caubut/features/neovim/nvim-config";
+  # xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/${config.home.username}/Documents/nixos-config/home/caubut/features/neovim/nvim-config";
 
   # home.file."./nixos-config/" = {
   #   source = ../../../../..;
