8848c7dd6062257a1b70c68eb2c796ba19b98d02
Christopher Aubut <christopher@aubut.me>
2024-01-05 20:15:06 -0700

    updates looks to be working

--- home/caubut/features/neovim/default.nix
+++ home/caubut/features/neovim/default.nix
@@ -18,14 +18,4 @@
   # home.file."./.config/nvim/".source = config.lib.file.mkOutOfStoreSymlink "nixos-config/home/caubut/features/neovim/nvim-config";
 }
 # NOTES":
-#   new ssh key for secrets
-#     copy to new persisted location
-#   remove /etc/nixos
-#   nvim-config submodule??
-#     usable on other systems
-#     use the source to keep synced with store
-#
-#   updates require updating submodule
-#     rebuild to be there next boot
-#
 #   or just keep one repo and symlink...
