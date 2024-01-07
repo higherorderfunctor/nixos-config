1d2fa8a6101ca8ff9000d4e904204b6fa59cc729
Christopher Aubut <christopher@aubut.me>
2024-01-07 00:20:26 -0700

    trying overload1

--- home/caubut/features/cli/zsh/default.nix
+++ home/caubut/features/cli/zsh/default.nix
@@ -33,37 +33,41 @@
         "hl+" = "#f38ba8";
       };
     };
-    oh-my-posh = {
+    programs.starship = {
       enable = true;
-      package = let
-        version = "19.4.0";
-        src = pkgs.fetchFromGitHub {
-          owner = "jandedobbeleer";
-          repo = "oh-my-posh";
-          rev = "refs/tags/v${version}";
-          # hash = lib.fakeSha256;
-          hash = "sha256-e3KYqCLbnjDKO4tiL/BssUmxmmsWJFqA1gOvwF9r7jo=";
-        };
-      in
-        pkgs.oh-my-posh.override (prev: {
-          buildGoModule = args:
-            pkgs.buildGoModule (args
-              // {
-                inherit version src;
-                # vendorHash = lib.fakeSha256;
-                vendorHash = "sha256-//L0tjM+JELglwCOWkifn39G4JuL/YBmJKBF1Uyno3M=";
-                postPatch = ''
-                  # these tests requires internet access
-                  rm engine/image_test.go \
-                    engine/migrate_glyphs_test.go \
-                    segments/nba_test.go
-                '';
-              });
-        });
-      settings =
-        builtins.fromJSON
-        (builtins.unsafeDiscardStringContext
-          (builtins.readFile ./catppuccin.omp.json));
+      settings = {};
     };
+    # oh-my-posh = {
+    #   enable = true;
+    #   package = let
+    #     version = "19.4.0";
+    #     src = pkgs.fetchFromGitHub {
+    #       owner = "jandedobbeleer";
+    #       repo = "oh-my-posh";
+    #       rev = "refs/tags/v${version}";
+    #       # hash = lib.fakeSha256;
+    #       hash = "sha256-e3KYqCLbnjDKO4tiL/BssUmxmmsWJFqA1gOvwF9r7jo=";
+    #     };
+    #   in
+    #     pkgs.oh-my-posh.override (prev: {
+    #       buildGoModule = args:
+    #         pkgs.buildGoModule (args
+    #           // {
+    #             inherit version src;
+    #             # vendorHash = lib.fakeSha256;
+    #             vendorHash = "sha256-//L0tjM+JELglwCOWkifn39G4JuL/YBmJKBF1Uyno3M=";
+    #             postPatch = ''
+    #               # these tests requires internet access
+    #               rm engine/image_test.go \
+    #                 engine/migrate_glyphs_test.go \
+    #                 segments/nba_test.go
+    #             '';
+    #           });
+    #     });
+    #   settings =
+    #     builtins.fromJSON
+    #     (builtins.unsafeDiscardStringContext
+    #       (builtins.readFile ./catppuccin.omp.json));
+    # };
   };
 }
