d3f8d53cd7a90f4a97583def98642d26bb118272
Christopher Aubut <christopher@aubut.me>
2024-01-06 22:17:18 -0700

    find fzf error

--- home/caubut/features/cli/zsh/default.nix
+++ home/caubut/features/cli/zsh/default.nix
@@ -15,16 +15,16 @@
       colors = {
         "bg+" = "#313244";
         bg = "#1e1e2e";
-        spinner = "#f5e0dc";
-        hl = "#f38ba8";
-        fg = "#cdd6f4";
-        header = "#f38ba8";
-        info = "#cba6f7,";
-        pointer = "#f5e0dc";
-        marker = "#f5e0dc";
-        "fg+" = "#cdd6f4";
-        prompt = "#cba6f7";
-        "hl+" = "#f38ba8";
+        # spinner = "#f5e0dc";
+        # hl = "#f38ba8";
+        # fg = "#cdd6f4";
+        # header = "#f38ba8";
+        # info = "#cba6f7,";
+        # pointer = "#f5e0dc";
+        # marker = "#f5e0dc";
+        # "fg+" = "#cdd6f4";
+        # prompt = "#cba6f7";
+        # "hl+" = "#f38ba8";
         # https://github.com/catppuccin/fzf
         # --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
         # --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
