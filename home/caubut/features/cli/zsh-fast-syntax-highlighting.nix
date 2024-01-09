6d215670a59276010b74d747d7b02321e9df83fa
Christopher Aubut <christopher@aubut.me>
2024-01-09 06:40:55 -0700

    fix: path

--- home/caubut/features/cli/zsh-fast-syntax-highlighting.nix
+++ home/caubut/features/cli/zsh-fast-syntax-highlighting.nix
@@ -5,26 +5,30 @@
   ...
 }: let
   configDir = "zsh/plugins/fast-syntax-highlighting";
-  fast-syntax-highlighting = builtins.fetchTarball {
-    url = "https://github.com/zdharma-continuum/fast-syntax-highlighting/archive/master.tar.gz";
-    # sha256 = lib.fakeSha256;
-    sha256 = "1bmrb724vphw7y2gwn63rfssz3i8lp75ndjvlk5ns1g35ijzsma5";
-  };
-  fast-syntax-highlighting-catppuccin = pkgs.stdenvNoCC.mkDerivation {
-    name = "zsh-fast-syntax-highlighting-catppuccin";
-
-    src = builtins.fetchTarball {
+  sources = {
+    fast-syntax-highlighting = builtins.fetchTarball {
+      url = "https://github.com/zdharma-continuum/fast-syntax-highlighting/archive/master.tar.gz";
+      # sha256 = lib.fakeSha256;
+      sha256 = "1bmrb724vphw7y2gwn63rfssz3i8lp75ndjvlk5ns1g35ijzsma5";
+    };
+    fast-syntax-highlighting-catppuccin = builtins.fetchTarball {
       url = "https://github.com/catppuccin/zsh-fsh/archive/master.tar.gz";
       # sha256 = lib.fakeSha256;
       sha256 = "1044pbfykcm16m7v3vwc6g1f9r3cxxmlaqch670yw6appbw62nfz";
     };
+  };
+  fast-syntax-highlighting = pkgs.stdenvNoCC.mkDerivation {
+    name = "zsh-fast-syntax-highlighting-catppuccin";
+
+    srcs = lib.attrValues sources;
 
     strictDeps = true;
     dontConfigure = true;
     dontBuild = true;
 
-    installPhase = ''
-      cp themes/* "$out"
+    unpackPhase = ''
+      cp ''${srcs[0]}/* "$out"
+      cp ''${srcs[1]}/* "$out/plugins/fast-syntax-highlighting/themes"
     '';
   };
 in {
@@ -34,10 +38,6 @@ in {
   '';
   xdg.configFile."${configDir}" = {
     source = fast-syntax-highlighting;
-    recursive = true;
-  };
-  xdg.configFile."${configDir}/themes" = {
-    source = fast-syntax-highlighting-catppuccin;
     recursive = true;
   };
 }
