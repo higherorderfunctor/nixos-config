9b055d04e3bb43d50e23475b506e6ec1cf96a09c
Christopher Aubut <christopher@aubut.me>
2024-01-09 05:19:04 -0700

    updates

--- home/caubut/features/cli/zsh-fast-syntax-highlighting.nix
+++ home/caubut/features/cli/zsh-fast-syntax-highlighting.nix
@@ -1,11 +1,16 @@
-_: let
+{config}: let
+  configDir = "zsh/plugins/fast-syntax-highlighting";
   fast-syntax-highlighting = builtins.fetchTarball {
     url = "https://github.com/zdharma-continuum/fast-syntax-highlighting/archive/master.tar.gz";
     # sha256 = lib.fakeSha256;
     sha256 = "1bmrb724vphw7y2gwn63rfssz3i8lp75ndjvlk5ns1g35ijzsma5";
   };
 in {
-  xdg.configFile."zsh/plugins/fast-syntax-highlighting" = {
+  programs.zsh.initExtra = ''
+    # fast-syntax-highlighting
+    source "${config.xdg.configDir}${configDir}/fast-syntax-highlighting.plugin.zsh"
+  '';
+  xdg.configFile."${configDir}" = {
     source = fast-syntax-highlighting;
     recursive = true;
   };
