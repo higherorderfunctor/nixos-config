592f373438ab2e9c45f4b75da6e0a773407b51b2
Christopher Aubut <christopher@aubut.me>
2024-01-08 19:05:35 -0700

    fix: zsh dotdir

--- home/caubut/features/cli/zsh.nix
+++ home/caubut/features/cli/zsh.nix
@@ -7,10 +7,10 @@
     enable = true;
     package = pkgs.zsh;
     defaultKeymap = "viins";
-    dotDir = "${config.xdg.configHome}/zsh";
-    # history = {
-    #   path = "${config.xdg.dataHome}/zsh/zsh_history";
-    # };
+    dotDir = ".config/zsh";
+    history = {
+      path = "${config.xdg.dataHome}/zsh/zsh_history";
+    };
   };
 }
 # oh-my-zsh = {
