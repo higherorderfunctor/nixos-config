d6ee43c38e22526533de1f3550b414f21cb521a1
Christopher Aubut <christopher@aubut.me>
2024-01-09 06:43:04 -0700

    fix: path

--- home/caubut/features/cli/zsh-fast-syntax-highlighting.nix
+++ home/caubut/features/cli/zsh-fast-syntax-highlighting.nix
@@ -4,21 +4,21 @@
   pkgs,
   ...
 }: let
-  configDir = "zsh/plugins/fast-syntax-highlighting";
+  configDir = "zsh/plugins/zsh-fast-syntax-highlighting";
   sources = {
-    fast-syntax-highlighting = builtins.fetchTarball {
+    zsh-fast-syntax-highlighting = builtins.fetchTarball {
       url = "https://github.com/zdharma-continuum/fast-syntax-highlighting/archive/master.tar.gz";
       # sha256 = lib.fakeSha256;
       sha256 = "1bmrb724vphw7y2gwn63rfssz3i8lp75ndjvlk5ns1g35ijzsma5";
     };
-    fast-syntax-highlighting-catppuccin = builtins.fetchTarball {
+    zsh-fast-syntax-highlighting-catppuccin = builtins.fetchTarball {
       url = "https://github.com/catppuccin/zsh-fsh/archive/master.tar.gz";
       # sha256 = lib.fakeSha256;
       sha256 = "1044pbfykcm16m7v3vwc6g1f9r3cxxmlaqch670yw6appbw62nfz";
     };
   };
-  fast-syntax-highlighting = pkgs.stdenvNoCC.mkDerivation {
-    name = "zsh-fast-syntax-highlighting-catppuccin";
+  zsh-fast-syntax-highlighting = pkgs.stdenvNoCC.mkDerivation {
+    name = "zsh-fast-syntax-highlighting";
 
     srcs = lib.attrValues sources;
 
@@ -28,7 +28,7 @@
 
     unpackPhase = ''
       cp ''${srcs[0]}/* "$out"
-      cp ''${srcs[1]}/* "$out/plugins/fast-syntax-highlighting/themes"
+      cp ''${srcs[1]}/* "$out/plugins/zsh-fast-syntax-highlighting/themes"
     '';
   };
 in {
@@ -37,7 +37,7 @@ in {
     source "${config.xdg.configHome}/${configDir}/fast-syntax-highlighting.plugin.zsh"
   '';
   xdg.configFile."${configDir}" = {
-    source = fast-syntax-highlighting;
+    source = zsh-fast-syntax-highlighting;
     recursive = true;
   };
 }
