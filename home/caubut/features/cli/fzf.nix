69bd6cadfd81010a3b0f3acc1e35f87dc04ed59f
Christopher Aubut <christopher@aubut.me>
2024-01-08 22:54:53 -0700

    updates

--- home/caubut/features/cli/fzf.nix
+++ home/caubut/features/cli/fzf.nix
@@ -9,9 +9,10 @@
     enableBashIntegration = config.programs.bash.enable;
     enableZshIntegration = config.programs.zsh.enable;
     colors = {
+      # TODO :use colors
       # https://github.com/catppuccin/fzf
-      #"bg+" = "#313244";
-      #bg = "#1e1e2e";
+      # "bg+" = "#313244";
+      # bg = "#1e1e2e";
       spinner = "#f5e0dc";
       hl = "#f38ba8";
       fg = "#cdd6f4";
