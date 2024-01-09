90615736185cb8b4500fa8b005a933d470b68735
Christopher Aubut <christopher@aubut.me>
2024-01-09 06:50:49 -0700

    fix: path

--- home/caubut/features/cli/zsh-fast-syntax-highlighting.nix
+++ home/caubut/features/cli/zsh-fast-syntax-highlighting.nix
@@ -11,7 +11,7 @@
       # sha256 = lib.fakeSha256;
       sha256 = "1bmrb724vphw7y2gwn63rfssz3i8lp75ndjvlk5ns1g35ijzsma5";
     };
-    zsh-fast-syntax-highlighting-catppuccin = builtins.fetchTarball {
+    fast-syntax-highlighting-catppuccin = builtins.fetchTarball {
       url = "https://github.com/catppuccin/zsh-fsh/archive/master.tar.gz";
       # sha256 = lib.fakeSha256;
       sha256 = "1044pbfykcm16m7v3vwc6g1f9r3cxxmlaqch670yw6appbw62nfz";
@@ -21,6 +21,8 @@
     name = "zsh-fast-syntax-highlighting";
 
     srcs = lib.attrValues sources;
+
+    sourceRoot = ".";
 
     strictDeps = true;
     dontConfigure = true;
