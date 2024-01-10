b84e7b9060da6b5bc3bad3a85a53d743f5cbaabf
Christopher Aubut <christopher@aubut.me>
2024-01-09 18:23:23 -0700

    fix: oh-my-posh config file

--- home/caubut/features/cli/less.nix
+++ home/caubut/features/cli/less.nix
@@ -1,7 +1,7 @@
 {
   programs.less.enable = true;
   home.sessionVariables = {
-    LESS = "LESS=-aRix2 --use-color --mouse --wheel-lines=3";
+    LESS = "-aRix2 --use-color --mouse --wheel-lines=3";
     # a = search from current position
     # i = case insensitive
     # x2 = tabstop
