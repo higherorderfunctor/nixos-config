190a276d98a2f6248817b23c5fb6e9377d5d7c11
Christopher Aubut <christopher@aubut.me>
2024-01-13 12:35:06 -0700

    update lock

--- home/caubut/features/neovim/default.nix
+++ home/caubut/features/neovim/default.nix
@@ -11,7 +11,7 @@
     source = ./nvim-config;
     recursive = true;
   };
-  activation = {
+  home.activation = {
     lazyVim = inputs.home-manager.lib.hm.dag.entryAfter ["writeBoundary"] ''
       $DRY_RUN_CMD nvim --headless "+Lazy! restore" +qa
     '';
