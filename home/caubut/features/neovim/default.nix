47bf58673f08a3110af7074047e4f3fc56c162f9
Christopher Aubut <christopher@aubut.me>
2024-01-13 12:55:22 -0700

    update lock

--- home/caubut/features/neovim/default.nix
+++ home/caubut/features/neovim/default.nix
@@ -7,13 +7,16 @@
   programs.neovim = {
     enable = true;
     package = pkgs.neovim-nightly;
+    extraPackages = with pkgs; [
+      gcc
+    ];
   };
   home.file."./.config/nvim/" = {
     source = ./nvim-config;
     recursive = true;
   };
   home.activation = {
-    lazyVim = inputs.home-manager.lib.hm.dag.entryAfter ["installPackages"] ''
+    nvim = inputs.home-manager.lib.hm.dag.entryAfter ["installPackages"] ''
       PATH="${config.home.path}/bin:$PATH" $DRY_RUN_CMD nvim --headless "+Lazy! restore" +qa
     '';
   };
