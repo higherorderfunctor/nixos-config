97dd4fbffa66afb54ac3a62c14d6aaeb5608585d
Christopher Aubut <christopher@aubut.me>
2024-01-13 17:24:16 -0700

    updates

--- home/caubut/features/neovim/default.nix
+++ home/caubut/features/neovim/default.nix
@@ -20,18 +20,18 @@ in {
     ];
   };
   xdg.configFile = {
-    nvim = {
-      source = builtins.filterSource (path: _: baseNameOf path != "lazy-lock.json") ./nvim-config;
-      recursive = true;
-    };
-    # "nvim/lazy-lock.json" = {
-    #   source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/nvim/lazy-lock.json";
+    # nvim = {
+    #   source = builtins.filterSource (path: _: baseNameOf path != "lazy-lock.json") ./nvim-config;
+    #   recursive = true;
     # };
+    "nvim/lazy-lock.json" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.userDirs.documents}/projects/nixos-config/home/${config.home.username}/nvim/features/neovim/nvim-config/lazy-lock.json";
+    };
   };
-  home.activation = {
-    nvim = inputs.home-manager.lib.hm.dag.entryAfter ["installPackages"] ''
-      ### cp ${config.xdg.configFile.nvim.source}/../lazy-lock.json ${config.xdg.configHome}/nvim/
-      PATH="${config.home.path}/bin:$PATH" $DRY_RUN_CMD nvim --headless "+Lazy! restore" +qa
-    '';
-  };
+  # home.activation = {
+  #   nvim = inputs.home-manager.lib.hm.dag.entryAfter ["installPackages"] ''
+  #     ### cp ${config.xdg.configFile.nvim.source}/../lazy-lock.json ${config.xdg.configHome}/nvim/
+  #     PATH="${config.home.path}/bin:$PATH" $DRY_RUN_CMD nvim --headless "+Lazy! restore" +qa
+  #   '';
+  # };
 }
