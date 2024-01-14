04f28db3426aff4139a443f491e8e09c980e7573
Christopher Aubut <christopher@aubut.me>
2024-01-14 16:57:42 -0700

    add hyprland

--- home/caubut/features/desktop/hyprland/default.nix
+++ home/caubut/features/desktop/hyprland/default.nix
@@ -4,19 +4,24 @@
   pkgs,
   ...
 }: {
+  xdg.portal = {
+    enable = true;
+    extraPortals = [inputs.hyprland.xdg-desktop-portal-hyprland];
+    configPackages = [inputs.hyprland.hyprland];
+  };
   wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     settings = {
       "$mod" = "SUPER";
     };
-    # systemd = {
-    #   enable = true;
-    #   # Same as default, but stop graphical-session too
-    #   extraCommands = lib.mkBefore [
-    #     "systemctl --user stop graphical-session.target"
-    #     "systemctl --user start hyprland-session.target"
-    #   ];
-    # };
+    systemd = {
+      enable = true;
+      # Same as default, but stop graphical-session too
+      extraCommands = lib.mkBefore [
+        "systemctl --user stop graphical-session.target"
+        "systemctl --user start hyprland-session.target"
+      ];
+    };
   };
 }
