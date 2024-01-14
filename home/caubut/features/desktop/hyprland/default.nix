e11f70e475a0c2d89a59f6a8a91c8cbbb9e671b6
Christopher Aubut <christopher@aubut.me>
2024-01-14 16:42:31 -0700

    add hyprland

--- home/caubut/features/desktop/hyprland/default.nix
+++ home/caubut/features/desktop/hyprland/default.nix
@@ -1,11 +1,22 @@
 {
   inputs,
+  lib,
   pkgs,
   ...
 }: {
   wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    settings = {};
+    settings = {
+      "$mod" = "SUPER";
+    };
+    # systemd = {
+    #   enable = true;
+    #   # Same as default, but stop graphical-session too
+    #   extraCommands = lib.mkBefore [
+    #     "systemctl --user stop graphical-session.target"
+    #     "systemctl --user start hyprland-session.target"
+    #   ];
+    # };
   };
 }
