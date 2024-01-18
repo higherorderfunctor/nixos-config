3df8960bace61768f761dc369f26ec5528287f2b
Christopher Aubut <christopher@aubut.me>
2024-01-17 17:59:37 -0700

    add hyprland

--- home/caubut/features/desktop/eww/default.nix
+++ home/caubut/features/desktop/eww/default.nix
@@ -9,6 +9,8 @@
     eww = {
       enable = true;
       package = inputs.eww.packages.${pkgs.system}.eww;
+      # Required to be set to something symlinked to .config/eww by the eww module.
+      # Not needed as .config/eww will be populated through xdg to load the files.
       configDir =
         lib.mkDefault
         (lib.strings.removePrefix
@@ -16,6 +18,7 @@
           "${config.xdg.configHome}/eww");
     };
   };
+  # override the symlink eww sets up
   xdg.configFile.eww = lib.mkForce {
     source = ./eww-config;
     recursive = true;
