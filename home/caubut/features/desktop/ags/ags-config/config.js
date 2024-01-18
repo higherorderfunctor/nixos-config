8ad5a3dc19226ad914cf2166c18540adfa3caa2a
Christopher Aubut <christopher@aubut.me>
2024-01-17 19:25:51 -0700

    add hyprland

--- home/caubut/features/desktop/ags/ags-config/config.js
+++ home/caubut/features/desktop/ags/ags-config/config.js
@@ -1,13 +1,28 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
+import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
+import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
 
-const myLabel = Widget.Label({
-    label: 'some example content',
+const focusedTitle = Widget.Label({
+    label: Hyprland.active.client.bind('title'),
+    visible: Hyprland.active.client.bind('address')
+        .transform(addr => !!addr),
 });
 
-const myBar = Widget.Window({
-    name: 'bar',
-    anchor: ['top', 'left', 'right'],
-    child: myLabel,
-});
+const dispatch = ws => Hyprland.sendMessage(`dispatch workspace ${ws}`);
 
-export default { windows: [myBar] };
+const Workspaces = () => Widget.EventBox({
+    onScrollUp: () => dispatch('+1'),
+    onScrollDown: () => dispatch('-1'),
+    child: Widget.Box({
+        children: Array.from({ length: 10 }, (_, i) => i + 1).map(i => Widget.Button({
+            attribute: i,
+            label: `${i}`,
+            onClicked: () => dispatch(i),
+        })),
+
+        // remove this setup hook if you want fixed number of buttons
+        setup: self => self.hook(Hyprland, () => box.children.forEach(btn => {
+            btn.visible = Hyprland.workspaces.some(ws => ws.id === btn.attribute);
+        })),
+    }),
+});
