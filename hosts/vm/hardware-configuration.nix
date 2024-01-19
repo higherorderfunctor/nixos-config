0aec47701b57c719b6d4c5dc8e311b7901ac8be2
Christopher Aubut <christopher@aubut.me>
2024-01-19 05:39:40 -0700

    attempt to fix startup config

--- hosts/vm/hardware-configuration.nix
+++ hosts/vm/hardware-configuration.nix
@@ -10,7 +10,12 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-  hardware.opengl.enable = true;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    # TODO all
+  };
 
   boot = {
     initrd = {
