edec99ed1d86dcf26d00b95ebd9cb3aac7e4f023
Christopher Aubut <christopher@aubut.me>
2024-01-20 12:27:55 -0700

    chore(firefox): install

--- home/caubut/features/desktop/firefox/default.nix
+++ home/caubut/features/desktop/firefox/default.nix
@@ -7,4 +7,13 @@
     enable = true;
     package = inputs.firefox-nightly.packages.${pkgs.system}.firefox-nightly-bin;
   };
+  home.packages = [
+    (pkgs.linkFarm "firefox" [
+      {
+        name = "firefox";
+        path = pkgs.firefox-nightly-bin;
+      }
+    ])
+  ];
+  # TODO: command-not-found db error
 }
