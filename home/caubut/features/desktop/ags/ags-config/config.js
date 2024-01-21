3f99ac26bc43f092e033cb1d5b331fcf2782cacd
Christopher Aubut <christopher@aubut.me>
2024-01-21 15:25:32 -0700

    chore(ags): tinkering

--- home/caubut/features/desktop/ags/ags-config/config.js
+++ home/caubut/features/desktop/ags/ags-config/config.js
@@ -1,4 +1,198 @@
-import p from"resource:///com/github/Aylur/ags/service/hyprland.js";import k from"resource:///com/github/Aylur/ags/service/notifications.js";import c from"resource:///com/github/Aylur/ags/service/mpris.js";import a from"resource:///com/github/Aylur/ags/service/audio.js";import f from"resource:///com/github/Aylur/ags/service/battery.js";import $ from"resource:///com/github/Aylur/ags/service/systemtray.js";import s from"resource:///com/github/Aylur/ags/app.js";import t from"resource:///com/github/Aylur/ags/widget.js";import{exec as W,execAsync as A}from"resource:///com/github/Aylur/ags/utils.js";import i from"resource:///com/github/Aylur/ags/widget.js";import h from"resource:///com/github/Aylur/ags/app.js";import _ from"resource:///com/github/Aylur/ags/service/applications.js";var l="applauncher",y=e=>i.Button({on_clicked:()=>{h.closeWindow(l),e.launch()},attribute:{app:e},child:i.Box({children:[i.Icon({icon:e.icon_name||"",size:42}),i.Label({class_name:"title",label:e.name,xalign:0,vpack:"center",truncate:"end"})]})}),w=({width:e=500,height:o=500,spacing:n=12})=>{let r=_.query("").map(y),b=i.Box({vertical:!0,children:r,spacing:n});function x(){r=_.query("").map(y),b.children=r}let m=i.Entry({hexpand:!0,css:`margin-bottom: ${n}px;`,on_accept:()=>{r[0]&&(h.toggleWindow(l),r[0].attribute.app.launch())},on_change:({text:d})=>r.forEach(u=>{u.visible=u.attribute.app.match(d)})});return i.Box({vertical:!0,css:`margin: ${n*2}px;`,children:[m,i.Scrollable({hscroll:"never",css:`
-                    min-width: ${e}px;
-                    min-height: ${o}px;
-                `,child:b})],setup:d=>d.hook(h,(u,v,B)=>{v===l&&B&&(x(),m.text="",m.grab_focus())})})},g=i.Window({name:l,popup:!0,visible:!1,focusable:!0,child:w({width:500,height:500,spacing:12})});import{monitorFile as j}from"resource:///com/github/Aylur/ags/utils.js";var L=()=>t.Box({class_name:"workspaces",children:p.bind("workspaces").transform(e=>e.map(({id:o})=>t.Button({on_clicked:()=>p.sendMessage(`dispatch workspace ${o}`),child:t.Label(`${o}`),class_name:p.active.workspace.bind("id").transform(n=>`${n===o?"focused":""}`)})))}),M=()=>t.Label({class_name:"client-title",label:p.active.client.bind("title")}),I=()=>t.Label({class_name:"clock",setup:e=>e.poll(1e3,o=>o.label=W('date "+%H:%M:%S %b %e."')).poll(1e3,o=>A(["date","+%H:%M:%S %b %e."]).then(n=>o.label=n))}),C=()=>t.Box({class_name:"notification",visible:k.bind("popups").transform(e=>e.length>0),children:[t.Icon({icon:"preferences-system-notifications-symbolic"}),t.Label({label:k.bind("popups").transform(e=>e[0]?.summary||"")})]}),S=()=>t.Button({class_name:"media",on_primary_click:()=>c.getPlayer("")?.playPause(),on_scroll_up:()=>c.getPlayer("")?.next(),on_scroll_down:()=>c.getPlayer("")?.previous(),child:t.Label("-").hook(c,e=>{if(c.players[0]){let{track_artists:o,track_title:n}=c.players[0];e.label=`${o.join(", ")} - ${n}`}else e.label="Nothing is playing"},"player-changed")}),N=()=>t.Box({class_name:"volume",css:"min-width: 180px",children:[t.Icon().hook(a,e=>{if(!a.speaker)return;let o={101:"overamplified",67:"high",34:"medium",1:"low",0:"muted"},n=a.speaker.is_muted?0:[101,67,34,1,0].find(r=>r<=a.speaker.volume*100);e.icon=`audio-volume-${o[n]}-symbolic`},"speaker-changed"),t.Slider({hexpand:!0,draw_value:!1,on_change:({value:e})=>a.speaker.volume=e,setup:e=>e.hook(a,()=>{e.value=a.speaker?.volume||0},"speaker-changed")})]}),P=()=>t.Box({class_name:"battery",visible:f.bind("available"),children:[t.Icon({icon:f.bind("percent").transform(e=>`battery-level-${Math.floor(e/10)*10}-symbolic`)}),t.ProgressBar({vpack:"center",fraction:f.bind("percent").transform(e=>e>0?e/100:0)})]}),D=()=>t.Box({children:$.bind("items").transform(e=>e.map(o=>t.Button({child:t.Icon({binds:[["icon",o,"icon"]]}),on_primary_click:(n,r)=>o.activate(r),on_secondary_click:(n,r)=>o.openMenu(r),binds:[["tooltip-markup",o,"tooltip-markup"]]})))}),E=()=>t.Box({spacing:8,children:[L(),M()]}),H=()=>t.Box({spacing:8,children:[S(),C()]}),T=()=>t.Box({hpack:"end",spacing:8,children:[N(),P(),I(),D()]}),q=(e=0)=>t.Window({name:`bar-${e}`,class_name:"bar",monitor:e,anchor:["top","left","right"],exclusivity:"exclusive",child:t.CenterBox({start_widget:E(),center_widget:H(),end_widget:T()})});j(`${s.configDir}/style.css`,function(){s.resetCss(),s.applyCss(`${s.configDir}/style.css`)});var oe={style:s.configDir+"/style.css",windows:[q(),g]};export{oe as default};
+import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
+import Notifications from 'resource:///com/github/Aylur/ags/service/notifications.js';
+import Mpris from 'resource:///com/github/Aylur/ags/service/mpris.js';
+import Audio from 'resource:///com/github/Aylur/ags/service/audio.js';
+import Battery from 'resource:///com/github/Aylur/ags/service/battery.js';
+import SystemTray from 'resource:///com/github/Aylur/ags/service/systemtray.js';
+import App from 'resource:///com/github/Aylur/ags/app.js';
+import Widget from 'resource:///com/github/Aylur/ags/widget.js';
+import { exec, execAsync } from 'resource:///com/github/Aylur/ags/utils.js';
+import { applauncher } from './app-launcher/index.js';
+
+// widgets can be only assigned as a child in one container
+// so to make a reuseable widget, make it a function
+// then you can simply instantiate one by calling it
+
+const Workspaces = () => Widget.Box({
+    class_name: 'workspaces',
+    children: Hyprland.bind('workspaces').transform(ws => {
+        return ws.map(({ id }) => Widget.Button({
+            on_clicked: () => Hyprland.sendMessage(`dispatch workspace ${id}`),
+            child: Widget.Label(`${id}`),
+            class_name: Hyprland.active.workspace.bind('id')
+                .transform(i => `${i === id ? 'focused' : ''}`),
+        }));
+    }),
+});
+
+const ClientTitle = () => Widget.Label({
+    class_name: 'client-title',
+    label: Hyprland.active.client.bind('title'),
+});
+
+const Clock = () => Widget.Label({
+    class_name: 'clock',
+    setup: self => self
+        // this is bad practice, since exec() will block the main event loop
+        // in the case of a simple date its not really a problem
+        .poll(1000, self => self.label = exec('date "+%H:%M:%S %b %e."'))
+
+        // this is what you should do
+        .poll(1000, self => execAsync(['date', '+%H:%M:%S %b %e.'])
+            .then(date => self.label = date)),
+});
+
+// we don't need dunst or any other notification daemon
+// because the Notifications module is a notification daemon itself
+const Notification = () => Widget.Box({
+    class_name: 'notification',
+    visible: Notifications.bind('popups').transform(p => p.length > 0),
+    children: [
+        Widget.Icon({
+            icon: 'preferences-system-notifications-symbolic',
+        }),
+        Widget.Label({
+            label: Notifications.bind('popups').transform(p => p[0]?.summary || ''),
+        }),
+    ],
+});
+
+const Media = () => Widget.Button({
+    class_name: 'media',
+    on_primary_click: () => Mpris.getPlayer('')?.playPause(),
+    on_scroll_up: () => Mpris.getPlayer('')?.next(),
+    on_scroll_down: () => Mpris.getPlayer('')?.previous(),
+    child: Widget.Label('-').hook(Mpris, self => {
+        if (Mpris.players[0]) {
+            const { track_artists, track_title } = Mpris.players[0];
+            self.label = `${track_artists.join(', ')} - ${track_title}`;
+        } else {
+            self.label = 'Nothing is playing';
+        }
+    }, 'player-changed'),
+});
+
+const Volume = () => Widget.Box({
+    class_name: 'volume',
+    css: 'min-width: 180px',
+    children: [
+        Widget.Icon().hook(Audio, self => {
+            if (!Audio.speaker)
+                return;
+
+            const category = {
+                101: 'overamplified',
+                67: 'high',
+                34: 'medium',
+                1: 'low',
+                0: 'muted',
+            };
+
+            const icon = Audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(
+                threshold => threshold <= Audio.speaker.volume * 100);
+
+            self.icon = `audio-volume-${category[icon]}-symbolic`;
+        }, 'speaker-changed'),
+        Widget.Slider({
+            hexpand: true,
+            draw_value: false,
+            on_change: ({ value }) => Audio.speaker.volume = value,
+            setup: self => self.hook(Audio, () => {
+                self.value = Audio.speaker?.volume || 0;
+            }, 'speaker-changed'),
+        }),
+    ],
+});
+
+const BatteryLabel = () => Widget.Box({
+    class_name: 'battery',
+    visible: Battery.bind('available'),
+    children: [
+        Widget.Icon({
+            icon: Battery.bind('percent').transform(p => {
+                return `battery-level-${Math.floor(p / 10) * 10}-symbolic`;
+            }),
+        }),
+        Widget.ProgressBar({
+            vpack: 'center',
+            fraction: Battery.bind('percent').transform(p => {
+                return p > 0 ? p / 100 : 0;
+            }),
+        }),
+    ],
+});
+
+const SysTray = () => Widget.Box({
+    children: SystemTray.bind('items').transform(items => {
+        return items.map(item => Widget.Button({
+            child: Widget.Icon({ binds: [['icon', item, 'icon']] }),
+            on_primary_click: (_, event) => item.activate(event),
+            on_secondary_click: (_, event) => item.openMenu(event),
+            binds: [['tooltip-markup', item, 'tooltip-markup']],
+        }));
+    }),
+});
+
+// layout of the bar
+const Left = () => Widget.Box({
+    spacing: 8,
+    children: [
+        Workspaces(),
+        ClientTitle(),
+    ],
+});
+
+const Center = () => Widget.Box({
+    spacing: 8,
+    children: [
+        Media(),
+        Notification(),
+    ],
+});
+
+const Right = () => Widget.Box({
+    hpack: 'end',
+    spacing: 8,
+    children: [
+        Volume(),
+        BatteryLabel(),
+        Clock(),
+        SysTray(),
+    ],
+});
+
+const Bar = (monitor = 0) => Widget.Window({
+    name: `bar-${monitor}`, // name has to be unique
+    class_name: 'bar',
+    monitor,
+    anchor: ['top', 'left', 'right'],
+    exclusivity: 'exclusive',
+    child: Widget.CenterBox({
+        start_widget: Left(),
+        center_widget: Center(),
+        end_widget: Right(),
+    }),
+});
+
+import { monitorFile } from 'resource:///com/github/Aylur/ags/utils.js';
+
+monitorFile(
+    `${App.configDir}/style.css`,
+    function() {
+        App.resetCss();
+        App.applyCss(`${App.configDir}/style.css`);
+    },
+);
+
+// exporting the config so ags can manage the windows
+export default {
+    style: App.configDir + '/style.css',
+    windows: [
+        Bar(),
+        applauncher
+
+        // you can call it, for each monitor
+        // Bar(0),
+        // Bar(1)
+    ],
+};
