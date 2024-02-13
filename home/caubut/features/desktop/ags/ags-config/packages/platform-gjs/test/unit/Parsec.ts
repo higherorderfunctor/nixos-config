4e50d8e7b644ec052f40d6424ba66e3a144c358e
Christopher Aubut <christopher@aubut.me>
2024-02-12 18:55:17 -0700

    feat: do stuff

--- home/caubut/features/desktop/ags/ags-config/packages/platform-gjs/test/unit/Parsec.ts
+++ home/caubut/features/desktop/ags/ags-config/packages/platform-gjs/test/unit/Parsec.ts
@@ -1,51 +1,75 @@
-import { Effect, Option, Types } from 'effect';
+import { Effect, flow, Option, pipe } from 'effect';
 
-export type ExtraInputError<A> = {
+export type ExtraInputError = {
   readonly _tag: 'ExtraInputError';
-  readonly parsed: A;
-  readonly extra: string;
+  readonly extraInput: string;
 };
 
-export type InsufficientInputError<A> = {
+export type InsufficientInputError = {
   readonly _tag: 'InsufficientInputError';
-  readonly parsed: A;
   readonly rest: string;
 };
 
-export type ParseError<A> = ExtraInputError<A> | InsufficientInputError<A>;
+export type ParseError = ExtraInputError | InsufficientInputError;
 
-export type MakeError<A = any> = {
-  (_tag: 'ExtraInputError', parsed: A, extra: string): ExtraInputError<A>;
-  (_tag: 'InsufficientInputError', parsed: A, rest: string): InsufficientInputError<A>;
+export type MakeError = {
+  (_tag: 'ExtraInputError', extra: string): ExtraInputError;
+  (_tag: 'InsufficientInputError', rest: string): InsufficientInputError;
 };
 
-export const makeError: MakeError<A> = <A>(_tag: ParseError<A>['_tag'], ...rest: any[]) => ({ _tag, ...rest });
+export const makeError: MakeError = <A>(_tag: ParseError['_tag'], ...rest: any[]) => ({ _tag, ...rest });
 
-export type Parser<A> = (source: string) => Effect.Effect<[A, Option.Option<string>], ParseError<A>>;
+export type Parser<A> = (source: string) => Effect.Effect<[A, Option.Option<string>], ParseError>;
 
-export const parse =
-  <T>(parser: Parser<T>) =>
-  (source: string) =>
-    parser(source);
+export const parse: <A>(parser: Parser<A>) => (source: string) => Effect.Effect<A, ParseError> = (parser) =>
+  flow(
+    parser,
+    Effect.map(([a]) => a),
+  );
 
-export const parseAll =
-  <T>(parser: Parser<T>) =>
-  (source: string) =>
-    parser(source).pipe(
-      Effect.flatMap(([a, extra]) =>
-        extra.pipe(
-          Option.map((extra) => Effect.fail(makeError('ExtraInputError', a, extra))),
-          Option.getOrElse(() => Effect.succeed(a)),
+export const parseAll: <A>(parser: Parser<A>) => (source: string) => Effect.Effect<A, ParseError> = (parser) =>
+  flow(
+    parser,
+    Effect.flatMap(([a, extra]) =>
+      extra.pipe(
+        Option.map((extra) => Effect.fail(makeError('ExtraInputError', extra))),
+        Option.getOrElse(() => Effect.succeed(a)),
+      ),
+    ),
+  );
+
+export const map: <A, B>(p: Parser<A>, f: (a: A) => B) => Parser<B> = (parser, f) =>
+  flow(
+    parser,
+    Effect.map(([a, s]) => [f(a), s]),
+  );
+
+export const flatMap: <A, B>(p: Parser<A>, f: (a: A) => Parser<B>) => Parser<B> = (parser, f) =>
+  flow(
+    parser,
+    Effect.flatMap(([a, s]) =>
+      s.pipe(
+        Option.map(f(a)),
+        Option.getOrElse(() =>
+          Effect.fail(
+            makeError(
+              'InsufficientInputError',
+              Option.getOrElse(s, () => ''),
+            ),
+          ),
         ),
       ),
-    );
+    ),
+  );
 
-// # bind :: Parser a -> (a -> Parser b) -> Parser b
-// # bind p f = Parser $ \s -> concatMap (\(a, s') -> parse (f a) s') $ parse p s
-// def bind(p: Parser[A], f: Callable[[A], Parser[B]]) -> Parser[B]:
-//     return lambda s: concat_map(lambda a, _s: f(a)(_s), p(s))
+export const isEmpty: (source: string) => Option.Option<string> = (source) =>
+  source === '' ? Option.none() : Option.some(source);
 
-export const flatMap =
-  <A, B>(p: Parser<A>, f: (a: A) => Parser<B>): Parser<B> =>
-  (s: string) =>
-    p(s).pipe(Effect.flatMap(([a, s]) => f(a)(s)));
+export const string: (s: string) => Parser<string> = (s) => (source) =>
+  source.startsWith(s)
+    ? Effect.succeed([s, isEmpty(source.slice(s.length))])
+    : Effect.fail(makeError('InsufficientInputError', source));
+
+type Char<S extends string> = S extends `${infer C}${infer _R}` ? C : S extends '' ? never : S;
+
+export const char: <C extends string>(c: Char<C>) => Parser<string> = string;
