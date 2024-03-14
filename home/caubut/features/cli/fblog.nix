9ed6b36f6dafd28a050291bdcb37c016a769adad
Christopher Aubut <christopher@aubut.me>
2024-03-14 02:23:53 -0600

    feat: notes

--- home/caubut/features/cli/fblog.nix
+++ home/caubut/features/cli/fblog.nix
@@ -1,10 +1,17 @@
+# _: (final: _: let
+#   nv = (import ./nvpkgs.nix).arkenfox;
+#   src = final.fetchFromGitHub {inherit (nv.src) owner repo rev sha256;};
+#   arkenfox = final.lib.readFile "${src}/user.js";
+# in {
+#   inherit arkenfox;
+# })
 {pkgs, ...}: let
   rustPlatform = pkgs.makeRustPlatform {
     cargo = pkgs.rust-bin.stable.latest.default;
     rustc = pkgs.rust-bin.stable.latest.default;
   };
   pname = "fblog";
-  version = "4.8.0";
+  version = "4.8.0"; # TODO version is not really loading
   src = pkgs.fetchFromGitHub {
     owner = "brocode";
     repo = pname;
