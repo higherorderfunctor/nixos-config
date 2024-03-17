1e2738310666ebb0027d65b14f31af723a7ee93b
Christopher Aubut <christopher@aubut.me>
2024-03-16 22:55:15 -0600

    chore: updates

--- overlays/firefox-nightly-overlay.nix
+++ overlays/firefox-nightly-overlay.nix
@@ -1,3 +1,34 @@
 {inputs, ...}: (final: _: {
-  firefox-nightly = inputs.firefox-nightly.packages.${final.system}.firefox-nightly-bin;
+  firefox-nightly =
+    inputs
+    .firefox-nightly
+    .packages
+    .${final.system}
+    .firefox-nightly-bin
+    .override {
+      # system-wide policies
+      extraPolicies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        DisablePocket = true;
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        DontCheckDefaultBrowser = true;
+        DisplayMenuBar = "default-off";
+        SearchBar = "separate";
+        ExtensionSettings = {
+          # uBlock Origin:
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+      };
+    };
 })
