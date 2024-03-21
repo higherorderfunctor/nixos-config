0a776e29b1309d520221caa1df71a36c26aa0f63
Christopher Aubut <christopher@aubut.me>
2024-03-21 11:34:22 -0600

    fix: sops fixes

--- home/caubut/features/cli/sops.nix
+++ home/caubut/features/cli/sops.nix
@@ -1,12 +1,23 @@
 {
   config,
   inputs,
+  lib,
   pkgs,
   ...
 }: {
   imports = [inputs.sops-nix.homeManagerModules.sops];
 
-  home.packages = [pkgs.sops];
+  home =
+    {
+      packages = [pkgs.sops];
+    }
+    # conditionally enabled on non-nixos systems
+    // lib.optionalAttrs config.targets.genericLinux.enable
+    {
+      activation.reload-secrets = config.lib.dag.entryAfter ["writeBoundary"] ''
+        /usr/bin/systemctl start --user sops-nix
+      '';
+    };
 
   sops = {
     gnupg.sshKeyPaths = [];
