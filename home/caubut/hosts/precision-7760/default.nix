9a03e8a5cd25399adfa5bfedcbb824de030eca98
Christopher Aubut <christopher@aubut.me>
2024-03-21 10:49:10 -0600

    chore: merge

--- home/caubut/hosts/precision-7760/default.nix
+++ home/caubut/hosts/precision-7760/default.nix
@@ -1,11 +1,19 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ../../global
     ../../features/cli/nix.nix
+    ../../features/system/nix-gl-host.nix
     ../../features/desktop
   ];
   # disable impermanence
   home.persistence = lib.mkForce {};
   # fix for non-nixos systems
   targets.genericLinux.enable = true;
+
+  # TODO:
+  home.packages = [pkgs.gnome-network-displays];
 }
