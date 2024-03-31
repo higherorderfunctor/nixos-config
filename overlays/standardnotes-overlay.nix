906420f0cfbae80c9d18b378fe5ccba5c903cfcd
Christopher Aubut <christopher@aubut.me>
2024-03-31 13:45:01 -0600

    chore: save

--- overlays/standardnotes-overlay.nix
+++ overlays/standardnotes-overlay.nix
@@ -5,7 +5,8 @@
   };
   nv = (import ./nvpkgs.nix)."standardnotes-${systemMap.${final.system}}-deb";
 in {
-  standardnotes = final.stdenvNoCC.mkDerivation {
+  # FIXME: https://discourse.nixos.org/t/standardnotes-3-191-4-fails-to-build/40934
+  standardnotes = final.stdenv.mkDerivation {
     inherit (nv) version;
 
     pname = nv.name;
@@ -21,21 +22,24 @@ in {
 
     nativeBuildInputs = with final; [makeWrapper dpkg desktop-file-utils];
 
-    unpackPhase = "dpkg-deb --fsys-tarfile $src | tar -x --no-same-permissions --no-same-owner";
+    unpackPhase = ''
+      dpkg-deb --fsys-tarfile $src | tar -x --no-same-permissions --no-same-owner
+      mv opt/Standard\ Notes opt/standardnotes
+    '';
 
     installPhase = ''
       runHook preInstall
 
-      mkdir -p $out/bin $out/share/standardnotes
-      cp -R usr/share/{applications,icons} $out/share
-      cp -R opt/Standard\ Notes/resources/app.asar $out/share/standardnotes/
+      mkdir -p "$out/bin" "$out/share/standardnotes"
+      cp -R usr/share/{applications,icons} "$out/share"
+      cp -R "opt/standardnotes/resources/app.asar" "$out/share/standardnotes/"
 
-      makeWrapper ${final.electron}/bin/electron $out/bin/standardnotes \
-        --add-flags $out/share/standardnotes/app.asar \
-        --prefix LD_LIBRARY_PATH : ${final.lib.makeLibraryPath (with final; [libsecret stdenv.cc.cc.lib])}
+       makeWrapper ${final.electron_27}/bin/electron "$out/bin/standardnotes" \
+         --add-flags "$out/share/standardnotes/app.asar" \
+         --prefix LD_LIBRARY_PATH : ${final.lib.makeLibraryPath (with final; [libsecret stdenv.cc.cc.lib])}
 
-      ${final.desktop-file-utils}/bin/desktop-file-install --dir $out/share/applications \
-        --set-key Exec --set-value standardnotes usr/share/applications/standard-notes.desktop
+       ${final.desktop-file-utils}/bin/desktop-file-install --dir "$out/share/applications" \
+         --set-key Exec --set-value standardnotes usr/share/applications/standard-notes.desktop
 
       runHook postInstall
     '';
