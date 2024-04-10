b01c49feb072cac5e43398c7511678bd25defa69
Christopher Aubut <christopher@aubut.me>
2024-04-10 09:46:55 -0600

    chore: updates

--- overlays/bun-overlay.nix
+++ overlays/bun-overlay.nix
@@ -1,12 +1,11 @@
-{lib, ...}: (final: prev: let
-  nv = (import ./nvpkgs.nix)."standardnotes-${final.system}";
+_: (final: prev: let
+  nv = (import ./nvpkgs.nix)."bun-${final.system}";
+  version = "${nv.version}-1";
 in {
   bun = prev.bun.overrideAttrs {
-    inherit (nv) version;
-    passthru.sources = lib.mkForce {
-      "${final.system}" = lib.fetchurl {
-        inherit (nv.src) url sha256;
-      };
+    inherit version;
+    src = final.fetchurl {
+      inherit (nv.src) url sha256;
     };
   };
 })
