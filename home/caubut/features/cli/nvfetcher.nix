49c1a4dd4693610e5d87ae45e91627408f41eecf
Christopher Aubut <christopher@aubut.me>
2024-04-11 17:57:05 -0600

    fix: updates and fix nvfetcher keyfile

--- home/caubut/features/cli/nvfetcher.nix
+++ home/caubut/features/cli/nvfetcher.nix
@@ -6,6 +6,7 @@
   home.packages = [pkgs.nvfetcher];
   sops.secrets = {
     "${config.home.username}-nvfetcher-keyfile" = {
+      path = "${config.xdg.configHome}/nvfetcher/keyfile.toml";
       mode = "400";
     };
   };
