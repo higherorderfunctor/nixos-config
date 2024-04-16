5737898bd67253240826eba3f853713fbb29bb85
Christopher Aubut <christopher@aubut.me>
2024-04-15 18:01:14 -0600

    feat(bruno): persistence and remove debug stuff

--- home/caubut/features/desktop/bruno.nix
+++ home/caubut/features/desktop/bruno.nix
@@ -1,3 +1,15 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home.packages = [pkgs.bruno];
+
+  # persistence
+  home.persistence = {
+    "/persist${config.home.homeDirectory}".directories = [
+      (lib.strings.removePrefix "${config.home.homeDirectory}/" "${config.xdg.configHome}/bruno")
+    ];
+  };
 }
