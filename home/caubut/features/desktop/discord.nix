f3cb8b4905d29aae1a56468ec6d2dcdbea081d4f
Christopher Aubut <christopher@aubut.me>
2024-04-15 18:20:15 -0600

    feat(discord): persistence

--- home/caubut/features/desktop/discord.nix
+++ home/caubut/features/desktop/discord.nix
@@ -1,3 +1,15 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home.packages = [pkgs.discord];
+
+  # persistence
+  home.persistence = {
+    "/persist${config.home.homeDirectory}".directories = [
+      (lib.strings.removePrefix "${config.home.homeDirectory}/" "${config.xdg.configHome}/discord")
+    ];
+  };
 }
