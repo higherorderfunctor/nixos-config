69d18d84b575fbba6574f8bdf1790e94e6169d52
Christopher Aubut <christopher@aubut.me>
2024-04-20 13:46:54 -0600

    chore: broken updates not applied

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,7 +12,7 @@
   (import ./nix-gl-host-overlay.nix {inherit inputs;})
   (import ./nvfetcher-overlay.nix {inherit inputs;})
   # (import ./nixd-overlay.nix {inherit inputs;})
-  (import ./oh-my-posh.nix {inherit inputs;})
+  (import ./oh-my-posh-overlay.nix {inherit inputs;})
   (import ./spectral-overlay.nix {inherit inputs;})
   (import ./standardnotes-overlay.nix {inherit inputs;})
   (import ./rust-overlay.nix {inherit inputs;})
