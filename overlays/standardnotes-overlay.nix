c3b8979dc767cc1cd605bb5b287a5eb317009635
Christopher Aubut <christopher@aubut.me>
2024-04-23 22:10:35 -0600

    chore: update

--- overlays/standardnotes-overlay.nix
+++ overlays/standardnotes-overlay.nix
@@ -1,9 +1,14 @@
 _: (final: prev: let
   nv = (import ./nvpkgs.nix)."standardnotes-desktop-deb-${final.system}";
+  electron = final.electron_27;
+  rpath = final.lib.makeLibraryPath (with final; [
+    libsecret
+    stdenv.cc.cc.lib
+  ]);
 in {
   standardnotes =
     (prev.standardnotes.override {
-      electron = final.electron_27;
+      inherit electron;
     })
     .overrideAttrs (attrs: {
       inherit (nv) version;
@@ -17,10 +22,27 @@ in {
         mv opt/Standard\ Notes opt/standardnotes
       '';
 
-      installPhase =
-        builtins.replaceStrings
-        ["Standard\\ Notes"]
-        ["standardnotes"]
-        attrs.installPhase;
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out/bin $out/share/standardnotes
+        cp -R usr/share/{applications,icons} $out/share
+        cp -R opt/standardnotes/resources/app.asar $out/share/standardnotes/
+
+        makeWrapper ${electron}/bin/electron $out/bin/standardnotes \
+          --add-flags $out/share/standardnotes/app.asar \
+          --prefix LD_LIBRARY_PATH : ${rpath}
+
+        desktop-file-install --dir $out/share/applications \
+          --set-key Exec --set-value standardnotes usr/share/applications/standard-notes.desktop
+
+        runHook postInstall
+      '';
+
+      # installPhase =
+      #   builtins.replaceStrings
+      #   ["Standard\\ Notes"]
+      #   ["standardnotes"]
+      #   attrs.installPhase;
     });
 })
