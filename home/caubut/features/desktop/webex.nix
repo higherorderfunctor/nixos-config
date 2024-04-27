25f5031b96b4c3985e35889b87bb407aefd7312a
Christopher Aubut <christopher@aubut.me>
2024-04-27 12:15:11 -0600

    chore: webex fixes

--- home/caubut/features/desktop/webex.nix
+++ home/caubut/features/desktop/webex.nix
@@ -4,16 +4,21 @@
   pkgs,
   ...
 }: {
-  # Run in xserver to login since webview is hidden
+  # Run in xserver to login since webview is hidden for initial setup
   # WAYLAND_DISPLAY= GDK_BACKEND=x11 webex
   home = {
-    packages = [pkgs.webex];
-    # (pkgs.webex.overrideAttrs
-    #   {
-    #     postPatch = ''
-    #       substituteInPlace opt/Webex/bin/webex.desktop --replace-fail /opt "env WAYLAND_DISPLAY= GDK_BACKEND=x11 $out/opt"
-    #     '';
-    #   })
+    # packages = [pkgs.webex];
+    packages = [
+      (pkgs.webex.overrideAttrs
+        {
+          # substituteInPlace opt/Webex/bin/webex.desktop --replace /opt $out/opt
+          postPatch = ''
+            substituteInPlace opt/Webex/bin/webex.desktop \
+              --replace-fail /opt $out/opt \
+              --replace-fail CiscoCollabHost "CiscoCollabHost $out/opt/Webex/lib/ libWebexAppLoader.so /Start"
+          '';
+        })
+    ];
 
     # WAYLAND_DISPLAY=:0 webex "$(readlink -f $(dirname $(readlink -f $(which webex)))/../lib)/" libWebexAppLoader.so /Start &
 
