dd04f6eb7d67664ea96f61bd06de48811229f3c9
Christopher Aubut <christopher@aubut.me>
2024-05-03 00:39:02 -0600

    chore: updates

--- home/caubut/features/cli/nvim/nvim-config/lua/plugins/code/lsp/copilot.lua
+++ home/caubut/features/cli/nvim/nvim-config/lua/plugins/code/lsp/copilot.lua
@@ -19,6 +19,9 @@ return {
         if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "%.env.*") then
           return false
         end
+        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "%secrets.*") then
+          return false
+        end
         return true
       end,
       typescript = true,
