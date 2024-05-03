7424ab02f8907e30d1701458abae49ba1a1339c0
Christopher Aubut <christopher@aubut.me>
2024-05-02 21:25:15 -0600

    chore: disable copilot for env

--- home/caubut/features/cli/nvim/nvim-config/lua/plugins/code/lsp/copilot.lua
+++ home/caubut/features/cli/nvim/nvim-config/lua/plugins/code/lsp/copilot.lua
@@ -15,7 +15,12 @@ return {
       markdown = true,
       nix = true,
       openapi = true,
-      sh = true,
+      sh = function()
+        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "%.env.*") then
+          return false
+        end
+        return true
+      end,
       typescript = true,
       yaml = true,
       ["*"] = false,
