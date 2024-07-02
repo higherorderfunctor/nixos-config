627a79e238844beba538854f5e19271b13990d7a
Christopher Aubut <christopher@aubut.me>
2024-07-02 16:05:20 -0600

    chore: updates

--- home/caubut/features/cli/nvim/nvim-config/lua/plugins/ui/notify.lua
+++ home/caubut/features/cli/nvim/nvim-config/lua/plugins/ui/notify.lua
@@ -1,6 +1,32 @@
+-- <c-b>	Scroll Backward	n, i, s
+-- <c-f>	Scroll Forward	n, i, s
+-- <leader>sn	  +noice	n
+-- <leader>sna	Noice All	n
+-- <leader>snd	Dismiss All	n
+-- <leader>snh	Noice History	n
+-- <leader>snl	Noice Last Message	n
+-- <leader>snt	Noice Picker (Telescope/FzfLua)	n
+-- <S-Enter>	Redirect Cmdline	c
+
+-- return {
+--   "rcarriga/nvim-notify",
+--   keys = {
+--     { "<leader>uN", "<cmd>Telescope notify<cr>", desc = "Show Notifications" },
+--   },
+-- }
 return {
-  "rcarriga/nvim-notify",
-  keys = {
-    { "<leader>uN", "<cmd>Telescope notify<cr>", desc = "Show Notifications" },
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  -- FIXME: REMOVE THIS once this issue is fixed: https://github.com/yioneko/vtsls/issues/159
+  opts = {
+    routes = {
+      {
+        filter = {
+          event = "notify",
+          find = "Node ComputedPropertyName was unexpected.",
+        },
+        opts = { skip = true },
+      },
+    },
   },
 }
