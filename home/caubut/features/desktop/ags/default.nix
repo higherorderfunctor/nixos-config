e11a7a86cd5c52cd256c9fea8a45fc2cc9facb2e
Christopher Aubut <christopher@aubut.me>
2024-11-15 19:59:47 -0700

    updates

--- home/caubut/features/desktop/ags/default.nix
+++ home/caubut/features/desktop/ags/default.nix
@@ -16,7 +16,7 @@ in {
   # symlink to clone of project to allow for easy editing
   xdg.configFile.ags.source =
     config.lib.file.mkOutOfStoreSymlink
-    "${config.xdg.userDirs.documents}/projects/nixos-config/home/${username}/features/desktop/ags/ags-config";
+    "${config.xdg.userDirs.documents}/projects/nixos-config/home/${username}/features/desktop/ags/take2";
   #  xdg.configFile.ags = lib.mkForce {
   #    source = ./ags-config;
   #    recursive = true;
