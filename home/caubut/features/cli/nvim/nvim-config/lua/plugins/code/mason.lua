a59e03db1b05c1f414308179f67f5bc4b5779955
Christopher Aubut <christopher@aubut.me>
2024-11-28 17:21:21 -0700

    chore: updates

--- home/caubut/features/cli/nvim/nvim-config/lua/plugins/code/mason.lua
+++ home/caubut/features/cli/nvim/nvim-config/lua/plugins/code/mason.lua
@@ -1,14 +1,14 @@
-local function set_minus(self, other)
-  local index = {}
-  for _, v in ipairs(other) do
-    index[v] = true
-  end
-  for i, v in ipairs(self) do
-    if index[v] then
-      table.remove(self, i)
-    end
-  end
-end
+-- local function set_minus(self, other)
+--   local index = {}
+--   for _, v in ipairs(other) do
+--     index[v] = true
+--   end
+--   for i, v in ipairs(self) do
+--     if index[v] then
+--       table.remove(self, i)
+--     end
+--   end
+-- end
 
 -- installed by nix
 local exclude_packages = {
@@ -26,41 +26,41 @@ local exclude_packages = {
 }
 
 -- patch to prevent excluded packages from being installed
-local mason_settings = require("mason.settings")
-mason_settings._set = mason_settings.set
----@diagnostic disable-next-line: duplicate-set-field
-mason_settings.set = function(opts)
-  set_minus(opts.ensure_installed, exclude_packages)
-  mason_settings._set(opts)
-end
-
-local mason_lspconfig_settings = require("mason-lspconfig.settings")
-mason_lspconfig_settings._set = mason_lspconfig_settings.set
----@diagnostic disable-next-line: duplicate-set-field
-mason_lspconfig_settings.set = function(opts)
-  set_minus(opts.ensure_installed, exclude_packages)
-  mason_lspconfig_settings._set(opts)
-end
+-- local mason_settings = require("mason.settings")
+-- mason_settings._set = mason_settings.set
+-- ---@diagnostic disable-next-line: duplicate-set-field
+-- mason_settings.set = function(opts)
+--   set_minus(opts.ensure_installed, exclude_packages)
+--   mason_settings._set(opts)
+-- end
+-- 
+-- local mason_lspconfig_settings = require("mason-lspconfig.settings")
+-- mason_lspconfig_settings._set = mason_lspconfig_settings.set
+-- ---@diagnostic disable-next-line: duplicate-set-field
+-- mason_lspconfig_settings.set = function(opts)
+--   set_minus(opts.ensure_installed, exclude_packages)
+--   mason_lspconfig_settings._set(opts)
+-- end
 
 return {
   --  :lua print(vim.inspect(require("mason.settings").current))
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      set_minus(opts.ensure_installed, exclude_packages)
-      opts.log_level = vim.log.levels.INFO
-      return opts
-    end,
-  },
-  --  :lua print(vim.inspect(require("mason-lspconfig.settings").current))
-  {
-    "williamboman/mason-lspconfig",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      set_minus(opts.ensure_installed, exclude_packages)
-      opts.log_level = vim.log.levels.INFO
-      return opts
-    end,
-  },
+--   {
+--     "williamboman/mason.nvim",
+--     opts = function(_, opts)
+--       opts.ensure_installed = opts.ensure_installed or {}
+--       --set_minus(opts.ensure_installed, exclude_packages)
+--       -- opts.log_level = vim.log.levels.INFO
+--       return opts
+--     end,
+--   },
+--   --  :lua print(vim.inspect(require("mason-lspconfig.settings").current))
+--   {
+--     "williamboman/mason-lspconfig",
+--     opts = function(_, opts)
+--       opts.ensure_installed = opts.ensure_installed or {}
+--       -- set_minus(opts.ensure_installed, exclude_packages)
+--       --opts.log_level = vim.log.levels.INFO
+--       return opts
+--     end,
+--   },
 }
