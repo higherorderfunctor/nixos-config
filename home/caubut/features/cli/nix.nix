d0104c419db1a4d24de3e6586b2f31c6c386b1a4
Christopher Aubut <christopher@aubut.me>
2025-01-17 15:40:51 -0700

    chore: builds but broken spectral

--- home/caubut/features/cli/nix.nix
+++ home/caubut/features/cli/nix.nix
@@ -3,27 +3,27 @@
   lib,
   ...
 }: {
-  nix = lib.mkMerge [
-    lib.shared.config.nix
-    {
-      # gc = {
-      #   frequency = "weekly";
-      # };
-      # extraOptions = ''
-      #   !include ${config.sops.secrets."${config.home.username}-nix-conf-secrets".path}
-      # '';
-    }
-  ];
-
-  sops.secrets = {
-    "${config.home.username}-nix-conf-secrets" = {
-      mode = "400";
-    };
-  };
-
-  home.persistence = {
-    "/persist${config.home.homeDirectory}".directories = [
-      (lib.strings.removePrefix "${config.home.homeDirectory}/" "${config.xdg.dataHome}/nix") # repl history
-    ];
-  };
+  #  nix = lib.mkMerge [
+  #    lib.shared.config.nix
+  #    {
+  #      gc = {
+  #        frequency = "weekly";
+  #      };
+  #      extraOptions = ''
+  #        !include ${config.sops.secrets."${config.home.username}-nix-conf-secrets".path}
+  #      '';
+  #    }
+  #  ];
+  #
+  #  sops.secrets = {
+  #    "${config.home.username}-nix-conf-secrets" = {
+  #      mode = "400";
+  #    };
+  #  };
+  #
+  #  home.persistence = {
+  #    "/persist${config.home.homeDirectory}".directories = [
+  #      (lib.strings.removePrefix "${config.home.homeDirectory}/" "${config.xdg.dataHome}/nix") # repl history
+  #    ];
+  #  };
 }
