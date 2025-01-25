5e52ef432692506a4e4eb7e46ed933cf54c6b80b
Christopher Aubut <christopher@aubut.me>
2025-01-25 00:24:48 -0700

    chore: updates

--- home/caubut/features/cli/awscli.nix
+++ home/caubut/features/cli/awscli.nix
@@ -1,3 +1,14 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home.packages = [pkgs.awscli2];
+
+  home.persistence = {
+    "/persist${config.home.homeDirectory}".directories = [
+      (lib.strings.removePrefix "${config.home.homeDirectory}/" "${config.home.homeDirectory}/.aws")
+    ];
+  };
 }
